718946c7698d04b1ba471413f9c1ceef74666a82
Tom Atkins <thomas.atkins@its.ny.gov>
2017-05-09 22:01:54 -0400

    changes to settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -730,7 +730,9 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  * will allow the site to run off of all variants of example.com and
  * example.org, with all subdomains included.
  */
-
+$settings['trusted_host_patterns'] = [
+  '^www\.highstrungpro\.com$','^localhost$', '^drupal8\.docksal$'
+];
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
